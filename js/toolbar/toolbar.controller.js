16d9d1ffd029a8bb74f0b6ec50f64e7b31344f23
andfwas <andfwas@gmail.com>
2017-11-30 10:20:43 -0700

    marked as read displays OK, no functionality yet

--- js/toolbar/toolbar.controller.js
+++ js/toolbar/toolbar.controller.js
@@ -2,21 +2,17 @@ angular.module('ngInbox')
   .controller('toolbar', toolbar)
   function toolbar() {
     const vm = this
-
     vm.test = function(mail) {
       for (var i = 0; i < mail.length; i++) {
         mail[i].selected = true
       }
     }
-
     vm.unselectAll = function(mail) {
       for (var i = 0; i < mail.length; i++) {
         mail[i].selected = false
       }
     }
-
     vm.selectAll = function(mail) {
-
       var count = 0
       for (var i = 0; i < mail.length; i++) {
         if (mail[i].selected == true) {
@@ -25,22 +21,13 @@ angular.module('ngInbox')
       }
       return count
     }
-
     vm.allLabels = [
-      {
-        id: 1,
-        label: 'dev'
-      },
-      {
-        id: 2,
-        label: 'personal'
-      },
-      {
-        id: 3,
-        label: 'gschool'
-      },
-    ]
-
+      { id: 1,
+        label: 'dev' },
+      { id: 2,
+        label: 'personal' },
+      { id: 3,
+        label: 'gschool' }]
     vm.addLabel = function(selected, mail) {
       for (var i = 0; i < mail.length; i++) {
         if (mail[i].labels.indexOf(selected) === -1 && mail[i].selected == true) {
@@ -49,7 +36,6 @@ angular.module('ngInbox')
         mail[i].labels.sort()
       }
     }
-
     vm.removeLabel = function(deleted, mail) {
       for (var i = 0; i < mail.length; i++) {
         if (mail[i].labels.indexOf(deleted) >= 0 && mail[i].selected == true) {
@@ -57,9 +43,6 @@ angular.module('ngInbox')
         }
       }
     }
-
-
-
     vm.markAsRead = function(mail) {
       for (var i = 0; i < mail.length; i++) {
         console.log(mail[i])
